716368873600e8e8981c9a7c52ae15e23d43dfe6
Tim <secoif@gmail.com>
2013-07-30 02:52:41 +0800

    Reorganise

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
+"use strict"
+
 var pull = require('pull-stream')
 
 var types = require('./types')
 
-module.exports = function(type, options) {
-  var cast = types[type]
-  if (!cast) return through()
-  return through(cast(options))
-}
-
-var through = pull.Through(function (read, map) {
+var through = pull.Through(function(read, map) {
+  var count = 0
   return function(end, cb) {
-    read(end, function (end, data) {
-      cb(end, map(data))
+    read(end, function(end, data) {
+      if (data === '') return cb(end, null)
+      cb(end, !end ? map(data) : null)
     })
   }
 })
+
+module.exports = function(type, options) {
+  var cast = types[type]
+  if (!cast) return pull.through()
+  return through(cast(options))
+}
+
+
